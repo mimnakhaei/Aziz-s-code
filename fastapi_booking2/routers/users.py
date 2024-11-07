5c5827c6162f34fbf98b110914f9ccc41151b119
AzizTuprak <aziz.abdirshid@gmail.com>
2024-11-07 18:12:32 +0100

    end user user updated with CRUD actions

--- fastapi_booking2/routers/users.py
+++ fastapi_booking2/routers/users.py
@@ -10,6 +10,7 @@ from db import db_users
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
+# Create user
 @router.post("/", response_model=UserDisplay)
 def create_user(request: UserCreate, db: Session = Depends(get_db)):
     hashed_password = Hash.bcrypt(request.password)
@@ -19,9 +20,27 @@ def create_user(request: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
+# Get a user
 @router.get("/{id}", response_model=UserDisplay)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(User).filter(User.id == id).first()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
+
+# Get all users
+@router.get("/", response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_users.get_all_users(db)
+
+# Update user
+@router.put("/{user_id}", response_model=UserDisplay)
+def update_user(user_id: int, user: UserCreate, db: Session = Depends(get_db)):
+    return db_users.update_user(db, user_id, user)
+    #return {"detail": "User updated successfully"}
+
+# Delete user
+@router.delete("/{user_id}")
+def delete_hotel(user_id: int, db: Session = Depends(get_db)):
+    db_users.delete_user(db, user_id)
+    return {"detail": "User deleted successfully"}
