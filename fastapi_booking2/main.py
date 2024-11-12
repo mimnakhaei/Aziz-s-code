0471a21131abc34e09e627bccf560be7401f894f
AzizTuprak <aziz.abdirshid@gmail.com>
2024-11-12 13:43:06 +0100

    hello router endpoint deleted

--- fastapi_booking2/main.py
+++ fastapi_booking2/main.py
@@ -7,19 +7,12 @@ from routers import users, hotels, rooms, reviews, bookings
 
 app = FastAPI()
 
+app.include_router(authentication.router)
 app.include_router(users.router)
 app.include_router(hotels.router)
 app.include_router(rooms.router)
 app.include_router(reviews.router)
 app.include_router(bookings.router)
-app.include_router(authentication.router)
 
 
-########
-
-@app.get("/hello")
-def index():
-    return {"Message": "Hello world"}
-
-###########
 Base.metadata.create_all(bind=engine)
