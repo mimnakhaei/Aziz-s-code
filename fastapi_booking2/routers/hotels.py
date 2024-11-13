59a32295b441a9f0d42c90b4e46ee7783bd02aea
AzizTuprak <aziz.abdirshid@gmail.com>
2024-11-13 16:06:37 +0100

    athontication added to the get_all_hotel function so that any user who is not admin role can't do this action

--- fastapi_booking2/routers/hotels.py
+++ fastapi_booking2/routers/hotels.py
@@ -25,7 +25,9 @@ def get_hotel(hotel_id: int, db: Session = Depends(get_db)):
     return hotel
 
 @router.get("/", response_model=List[HotelDisplay])
-def get_all_hotels(db: Session = Depends(get_db)):
+def get_all_hotels(db: Session = Depends(get_db), current_user = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(403, "You're not allowed to do this")
     return db_hotels.get_all_hotels(db)
 
 #  
