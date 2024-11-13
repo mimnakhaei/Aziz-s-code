c8fbb68972f6f03f5944933fbb481b899a1247c6
AzizTuprak <aziz.abdirshid@gmail.com>
2024-11-13 14:45:37 +0100

    fixed

--- fastapi_booking2/auth/authentication.py
+++ fastapi_booking2/auth/authentication.py
@@ -11,7 +11,7 @@ router = APIRouter(
     tags=["authentication"]
 )
 
-@router.post("/Login")
+@router.post("/token")
 def get_token(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.username == request.username).first()
     if not user:
